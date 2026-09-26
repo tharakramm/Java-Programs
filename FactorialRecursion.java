cd15fc8c5b9b146eafd05732e3f06edc71a0177c
tharakramm <tharak974@gmail.com>
2026-09-26 19:05:40 +0530

    Create FactorialRecursion.java

---
+++ FactorialRecursion.java
@@ -0,0 +1,37 @@
+class FactorialRecursion
+{
+    int fact(int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n*fact(n-1);
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        int n,v;
+        
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter n :");
+        n=sc.nextInt();
+        
+        if(n<0)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+        
+        FactorialRecursion r=new FactorialRecursion();
+        
+        v=r.fact(n);
+        
+        System.out.println("Factorial of "+n+" = "+v);
+        
+    }
+}
